516b52dd60450b49f5ef3fb2d88497b31215d71e
Juan Jesús Ortiz Vazquez <juanjesusov@gmail.com>
2021-10-08 11:26:12 -0500

    entrega 2
    llenadoManual: Funciona correctamente
    llenadoAtomatico: Funciona correctamente
    estadoGanador: Preguntar por el estado ganador correcto
    desplegarTablero: Funciona correctamente
    main: Funciona correctamente

--- proyectoIntegrador.py
+++ proyectoIntegrador.py
@@ -1,90 +1,131 @@
-#proyecto integrador
-#los numeros ingresados o generados tienen que ser del 1 al 15
-#Para el espacio transformar el cero en string y después a '(espacio)' 
-#buscar duplicados if x not in []
+#PENDIENTES
+#deslizar
 
 def llenadoManual(matriz):
-    for i in range (4):
-        matriz[i] = input().strip()
-        matriz[i] = matriz[i].split(' ')
-        for j in range(4):
-            matriz[i][j] = int(matriz[i][j])
-            while (matriz[i][j] > 16) or (matriz[i][j] < 0):
-                print("Error. Valores fuera de rango")
-                matriz[i] = input().strip()
-                matriz[i] = matriz[i].split(' ')
-                matriz[i][j] = int(matriz[i][j])
-
-    #No la necesitamos
-    """for i in range (len(matriz)):
+    """
+        Estandar de codificación:
+            Nombre de Función: llenadoManual
+            Descripción: Pide al usuario escribir 16 números entre 0 y 15 sin repetirse.
+            Parámetros de entrada: matriz
+            Parámetros de salida: matriz
+        Autores: 
+            Ricardo López
+            Juan Jesús Ortiz
+    """
+    lista = []
+    for i in range (len(matriz)):
         for j in range(len(matriz[i])):
-            matriz[i][j] = int(matriz[i][j])"""
-    
-    if (len(matriz[0]) or len(matriz[1]) or len(matriz[0]) or len(matriz[1])) != 4:
-        matriz = "ERROR: Solo se permiten 4 columnas"
-        exit()
+            num = int(input())
+            
+            while (num > 15) or (num < 0):
+                print("ERROR: Valor fuera de rango")
+                num = int(input())
+            
+            while num in lista:
+                print("ERROR: No se puede repetir los números")
+                num = int(input())
 
-    """copias = [[],[],[],[]]
-    for i in range(4):
-        copias[i] = set(matriz[i])
-    print("copias ",copias,"len",len(copias))"""
-    
-    """for x in matriz:
-        for y in matriz[x]:
-            if matriz.count(y)!=1:
-                matriz="ERROR: Ingresó algún valor duplicado"""
-    
-    """if (len(matriz) != len(copias)):
-        matriz = "ERROR: Ingreso algún valor duplicado"
-        exit()"""
+            lista.append(num)
+            matriz[i][j] = num
 
     return matriz
 
 def llenadoAutomatico(matriz):
+    """
+        Estandar de codificación:
+            Nombre de Función: llenadoAutomatico
+            Descripción: Llena la matriz con números aleatorios entre 0 y 15 sin repetirse.
+            Parámetros de entrada: matriz
+            Parámetros de salida: matriz
+        Autores: 
+            Ricardo López
+            Juan Jesús Ortiz
+    """
     from random import randint
 
-    """for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            numRand = randint(0,16)
-            repetido = True    
-            for x in range(len(matriz)):
-                for y in range(len(matriz[x])):
-                    if numRand != matriz[x][y]:
-                        repetido=False"""
-            
     lista=[]
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            numRand = randint(0,16)
-            while (numRand not in lista) and (len(lista)<17):
-                numRand = randint(0,16)
-                lista.append(numRand)
-                matriz[i][j] = numRand
-    print(lista)
+    for r in range(len(matriz)):
+        for c in range(len(matriz[r])):
+            numRand = randint(0, 15)
+            
+            while numRand in lista:
+                numRand = randint(0, 15)
+            
+            lista.append(numRand)
+            matriz[r][c] = numRand
+    
     return matriz
 
+def estadoGanador(matriz):
+    """
+        Estandar de codificación:
+            Nombre de Función: estadoGanador
+            Descripción: Revisa si la matriz esta en orden, y si lo está, felicita al usuario.
+            Parámetros de entrada: matriz
+            Parámetros de salida: ganador
+        Autores: 
+            Ricardo López
+            Juan Jesús Ortiz
+    """
+    #¿La matriz ganadora donde tiene que tener al 0?
+    matrizGanadora1 = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]]
+    matrizGanadora2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
+    ganador = False
+    if matriz == matrizGanadora1 or matriz == matrizGanadora2:
+        ganador = True
+
+    return ganador
+
 def desplegarTablero(matriz):
-    print(matriz,"\n")
-    
+    """
+        Estandar de codificación:
+            Nombre de Función: desplegarTablero
+            Descripción: Imprime el tablero en forma de tabla.
+            Parámetros de entrada: matriz
+            Parámetros de salida: matriz
+        Autores: 
+            Ricardo López
+            Juan Jesús Ortiz
+    """
     for i in range (len(matriz)):
         for j in range(len(matriz[i])):
+            
+            if matriz[i][j] == 0: 
+                matriz[i][j] = str(matriz[i][j])
+                matriz[i][j] = ""
+
             print(matriz[i][j], end="\t")
+
         print()
     
     return matriz
 
 def main(matriz):
-    #preguntar el nombre
-    decision = input("¿Desea llenar la tabla manualmente o que el programa lo haga automatico por usted? (m/a): ")
+    """
+        Estandar de codificación:
+            Nombre de Función: main
+            Descripción: Se encarga de solicitar al usuario si es que quiere llenar su tablero el mismo, o que se haga automatico,
+                         además se encarga de llamar a otras funciones según sea pertinente.
+            Parámetros de entrada: matriz
+            Parámetros de salida: -
+        Autores: 
+            Ricardo López
+            Juan Jesús Ortiz
+    """
+    nombre = input("Ingresa tu nombre: ")
+    decision = input("\n¿Desea llenar la tabla manualmente o que el programa lo haga automatico por usted? (m/a): ")
     if decision=='m':
-        print("Ingrese las filas separadas por espacios")
+        print("\nPara indicar el espacio vacio ingrese cero")
         matriz = llenadoManual(matriz)
     else:
         llenadoAutomatico(matriz)
 
-    print("El numero cero indica la casilla vacia")
-    desplegarTablero(matriz)
+    ganador = estadoGanador(matriz)
 
-#matriz = [[0]*4]*4 ¿Porque se repiten despues de llenarse?
+    desplegarTablero(matriz)
+    
+    if ganador==True:
+        print("¡Felicidades {}, haz ganado!".format(nombre))
+
 matriz = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 main(matriz)
